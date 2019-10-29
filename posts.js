03cd646cfb3b8e4c2e886600c87d1336df9ac7bb
DimejiAre <olardi2@gmail.com>
2019-10-29 14:45:06 +0100

    add endpoints to get posts by id and update posts

--- posts.js
+++ posts.js
@@ -21,6 +21,25 @@ router.get('/', (req,res) => {
         })
 })
 
+router.get('/:id', (req,res) => {
+    const id = req.params.id;
+    db.findById(id)
+        .then(posts => {
+            if(posts.length > 0){
+                res.status(200).json(posts)
+            } else {
+                res.status(404).json({
+                    message: "The post with the specified ID does not exist."
+                })
+            }
+        })
+        .catch(()=>{
+            res.status(500).json({
+                error: "The post information could not be retrieved."
+            })
+        })
+})
+
 router.post('/', (req,res) => {
     const post = req.body
     if(post.title && post.contents){
@@ -40,6 +59,55 @@ router.post('/', (req,res) => {
             errorMessage: "Please provide title and contents for the post."
         })
     }
+})
+
+router.put('/:id', (req,res) => {
+    const id = req.params.id
+    const post = req.body
+    if(post.title && post.contents){
+        db.update(id,post)
+            .then(data => {
+                if(data){
+                    db.findById(id).then(updatedUser => {
+                        res.status(200).json(updatedUser[0])
+                    })
+                } else {
+                    res.status(404).json({
+                        message: "The post with the specified ID does not exist."
+                    })
+                }
+            })
+            .catch(() => {
+                res.status(500).json({
+                    error: "There was an error while saving the post to the database"
+                })
+            })
+    } else {
+        res.status(400).json({
+            errorMessage: "Please provide title and contents for the post."
+        })
+    }
+})
+
+router.delete('/:id', (req,res) => {
+    const id = req.params.id;
+    db.remove(id)
+        .then(data => {
+            if(data){
+                res.status(200).json({
+                    message: "post deleted"
+                })
+            } else {
+                res.status(404).json({
+                    message: "The post with the specified ID does not exist."
+                })
+            }
+        })
+        .catch(()=> {
+            res.status(500).json({
+                error: "The post could not be removed"
+            })
+        })
 })
 
 module.exports = router;
